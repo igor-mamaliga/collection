3b60baf73d490df81b7e3327eb1ec7e76d19dfed
igor-mamaliga <igor.mamaliga302@gmail.com>
2020-12-21 21:01:38 +0100

    - added EventHub sender/receiver

---
+++ Azure/EventHub/Receiver/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading;
+
+namespace Receiver
+{
+    internal class Program
+    {
+        private const string EventHubConnectionString =
+            @"Endpoint=sb://event-hub-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=*******************";
+        private const string EventHubName = "event-hub-432423";
+        private const string StorageContainerName = "container";
+        private static readonly string StorageConnectionString =
+            @"DefaultEndpointsProtocol=https;AccountName*********;AccountKey=******;EndpointSuffix=core.windows.net";
+
+        static void Main(string[] args)
+        {
+            List<EventProcessorHost> eventProcessorHosts = new List<EventProcessorHost>();
+                var eventProcessorHost = new EventProcessorHost(
+                    EventHubName,
+                    PartitionReceiver.DefaultConsumerGroupName,
+                    EventHubConnectionString,
+                    StorageConnectionString,
+                StorageContainerName);
+                eventProcessorHosts.Add(eventProcessorHost);
+            eventProcessorHosts[0].RegisterEventProcessorAsync<SimpleEventProcessor>();
+            
+            Console.WriteLine("Receiving.Press ENTER to stop worker.");
+            Console.ReadKey(false);
+            eventProcessorHosts.ForEach(h =>
+            {
+                // Disposes of the Event Processor Host        
+                h.UnregisterEventProcessorAsync();
+            });
+        }
+    }
+
+    public class SimpleEventProcessor : IEventProcessor
+    {
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down. Partition' {context.PartitionId}', Reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}' thread:{Thread.CurrentThread.ManagedThreadId}");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($"Error on Partition: {context.PartitionId}, Error:            {error.Message}");
+            return Task.CompletedTask;
+        }
+
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                                                                                                                     
+                Console.WriteLine($"messages count: {messages.Count()} Message received. Partition: '{context.PartitionId}', Data: '{data}',  thread:{Thread.CurrentThread.ManagedThreadId}");
+            }
+            //     Writes the current offset and sequenceNumber to the checkpoint store via the
+            //     checkpoint manager.
+            return context.CheckpointAsync();
+        }
+    }
+}
---
+++ Azure/EventHub/Sender/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        static EventHubClient eventHubClient;
+        const string EventHubConnectionString = @"Endpoint=sb://event-hub-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=*******************";
+        const string EventHubName = "event-hub";
+        const int numMessagesToSend = 8000;
+        const int sleepMilliseconds = 2000;
+
+
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid().ToString().Substring(12, 4);
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+            
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}, sender id:[{id}] {DateTime.Now.ToLongTimeString()}";
+                    List<EventData> e = new List<EventData>();
+                    var data = new EventData(Encoding.UTF8.GetBytes(message));
+                    e.Add(data);
+                    eventHubClient.SendAsync(e).Wait();
+                    Console.WriteLine($"Send: {message}");
+                    if (sleepMilliseconds > 0)
+                        Thread.Sleep(sleepMilliseconds);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+            eventHubClient.CloseAsync();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(false);
+        }
+    }
+}
